ba531b484e5cd8000bfd68ab26e64c0d7d3dad04
yang <yang@woshiyang.com>
2023-12-18 09:47:50 +0800

    update

--- Android/hook_ssl.js
+++ Android/hook_ssl.js
@@ -1,15 +1,35 @@
+function hook_checkServerTrusted() {
+    var ClassName = "com.android.org.conscrypt.Platform";
+    var Platform = Java.use(ClassName);
+    var targetMethod = "checkServerTrusted";
+    var len = Platform[targetMethod].overloads.length;
+    console.log(len);
+    for (var i = 0; i < len; ++i) {
+        Platform[targetMethod].overloads[i].implementation = function () {
+            console.log("class:", ClassName, "target:", targetMethod, " i:", i, arguments);
+        };
+    }
+}
+
+function hook_checkTrustedRecursive() {
+    var ClassName = "com.android.org.conscrypt.TrustManagerImpl";
+    var Platform = Java.use(ClassName);
+    var targetMethod = "checkTrustedRecursive";
+    var ArrayList = Java.use("java.util.ArrayList");
+
+    var len = Platform[targetMethod].overloads.length;
+    console.log(len);
+    for (var i = 0; i < len; ++i) {
+        Platform[targetMethod].overloads[i].implementation = function () {
+            console.log("class:", ClassName, "target:", targetMethod, " i:", i, arguments);
+            return ArrayList.$new(); 
+        };
+    }
+}
 function hook_ssl() {
     Java.perform(function () {
-        var ClassName = "com.android.org.conscrypt.Platform";
-        var Platform = Java.use(ClassName);
-        var targetMethod = "checkServerTrusted";
-        var len = Platform[targetMethod].overloads.length;
-        console.log(len);
-        for (var i = 0; i < len; ++i) {
-            Platform[targetMethod].overloads[i].implementation = function () {
-                console.log("class:", ClassName, "target:", targetMethod, " i:", i, arguments);
-            };
-        }
+        hook_checkServerTrusted();
+        hook_checkTrustedRecursive();
     });
 }
 
